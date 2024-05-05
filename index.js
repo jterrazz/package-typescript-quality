e4d8004e7b879df5204c7f7e7309fc2b8d989614
Jean-Baptiste Terrazzoni <j2aa@pm.me>
2024-05-05 13:45:51 +0200

    chore: use src folder structure

--- index.js
+++
@@ -1,4 +0,0 @@
-import eslintNodeConfig from './src/eslint/node.js';
-import prettierConfig from './src/prettier/index.js';
-
-export { eslintNodeConfig, prettierConfig };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import eslintNodeConfig from './eslint/node.js';
+import prettierConfig from './prettier/index.js';
+
+export { eslintNodeConfig, prettierConfig };
