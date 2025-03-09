a2d838794091d603fb9f5bc9db169849b8709f81
Jean-Baptiste Terrazzoni <j2aa@pm.me>
2025-03-09 16:14:35 +0100

    feat: add no-unused-expressions rule with flexible configuration

--- src/eslint/common.js
+++ src/eslint/common.js
@@ -17,6 +17,18 @@ export default [
 
     // Configuration for Typescript rules
     ...ts.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-unused-expressions': [
+                'error',
+                {
+                    allowShortCircuit: true,
+                    allowTernary: true,
+                    allowTaggedTemplates: true,
+                },
+            ],
+        },
+    },
 
     // Configuration for SonarJS
     {
